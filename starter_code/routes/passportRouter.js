0d656f791cee8bcfe0f703f40c06b2bfdd16bfb7
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-09-17 18:18:00 +0200

    Sign up and log in pages done

--- starter_code/routes/passportRouter.js
+++ starter_code/routes/passportRouter.js
@@ -14,4 +14,22 @@ router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
 
+router.get("/signup", (req, res, next) => {
+  res.render("./passport/signup")
+})
+
+router.post("/signup", (req, res, next) => {
+  let username = req.params.username;
+  let password = req.params.password;
+  {username, password }
+})
+
+router.get("/login", (req, res, next) => {
+  res.render("./passport/login")
+})
+
+router.post("/login", (req, res, next) => {
+
+})
+
 module.exports = router;
